80f46a8a32c175d35e0d0072eb9bc50da01ff485
Fredson Rodrigues <fredson.rodrigues.principal@gmail.com>
2020-02-02 14:10:04 -0300

    Testando fluxo de crud

--- server/Clients/Model/Client.js
+++ server/Clients/Model/Client.js
@@ -1,9 +1,9 @@
-const mongoose = require(`../../../lib/mongo`)
+const { Schema, model } = require(`../../../lib/mongo`)
 
-var clientSchema = new mongoose.Schema({
+var clientSchema = new Schema({
     name: {type: String, required: true},
     email: {type: String, unique: true, required: true}
 });
 
-var Client = mongoose.model('Client', clientSchema);
+var Client = model('Client', clientSchema);
 module.exports = Client
--- test/api.test.js
+++ test/api.test.js
@@ -4,9 +4,13 @@ const Lab = require('@hapi/lab');
 const { expect } = require('@hapi/code');
 const { after, before, describe, it } = exports.lab = Lab.script();
 const { init } = require('../lib/server');
+const Client = require('../server/Clients/Model/Client');
 
 describe('GET /', () => {
     let server;
+    let test = {
+        _id: 0
+    }
 
     before(async () => {
         server = await init();
@@ -24,35 +28,48 @@ describe('GET /', () => {
         expect(res.statusCode).to.equal(200);
     });
 
+    it('responds /Clients POST with 200', async () => {
+        const res = await server.inject({
+            method: 'post',
+            url: '/clients',
+            payload: {
+                name: 'Marcus Tester',
+                email: 'tester@teste.com'
+            }
+        });
+        expect(res.statusCode).to.equal(200);
+        expect(JSON.parse(res.payload)).to.be.an.instanceof(Object);
+        test = JSON.parse(res.payload)
+    });
+
     it('responds /Clients GET with 200', async () => {
         const res = await server.inject({
             method: 'get',
             url: '/clients'
         });
         expect(res.statusCode).to.equal(200);
-    });
-
-    it('responds /Clients POST with 200', async () => {
-        const res = await server.inject({
-            method: 'post',
-            url: '/clients'
-        });
-        expect(res.statusCode).to.equal(200);
+        expect(JSON.parse(res.payload)).to.be.an.instanceof(Array);
     });
 
     it('responds /Clients PUT with 200', async () => {
         const res = await server.inject({
             method: 'put',
-            url: '/clients/1'
+            url: `/clients/${test._id}`,
+            payload: {
+                ...test,
+                name: 'Teste teste'
+            }
         });
         expect(res.statusCode).to.equal(200);
+        expect(JSON.parse(res.payload)).to.be.an.instanceof(Object);
     });
 
     it('responds /Clients DELETE with 200', async () => {
         const res = await server.inject({
             method: 'delete',
-            url: '/clients/1'
+            url: `/clients/${test._id}`
         });
+        expect(JSON.parse(res.payload)).to.be.an.instanceof(Object);
         expect(res.statusCode).to.equal(200);
     });
 });
