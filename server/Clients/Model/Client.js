bb0b289411a1e1be55e4c6c4c092e6d514e3d37f
Fredson Rodrigues <fredson.rodrigues.principal@gmail.com>
2020-02-08 11:20:56 -0300

    Criando hook para retornar objeto da lista de desejo

--- server/Clients/Model/Client.js
+++ server/Clients/Model/Client.js
@@ -1,8 +1,29 @@
 const { Schema, model } = require(`../../../lib/mongo`)
+const axios = require('axios')
 
 var clientSchema = new Schema({
-    name: {type: String, required: true},
-    email: {type: String, unique: true, required: true}
+    name: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
+    favorites: { type: Array }
+});
+
+clientSchema.post('find', function (doc) {
+    doc.map((e, k) => {
+        const promises = e.favorites.map(fav => new Promise((resolve, reject) => {
+            var url = `http://challenge-api.luizalabs.com/api/product/${fav}`;
+            axios(url).then((response) => {
+                return response.data;
+            })
+            .catch((error) => {
+                return error;
+            })
+        }));
+
+        Promise.all(promises).then(results => {
+            console.log(results)
+            doc[k].favorites = results
+        });
+    })
 });
 
 var Client = model('Client', clientSchema);
