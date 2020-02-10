dddbe4239597f5ccd2282e64a6cd7f951de40c16
Fredson Rodrigues <fredson.rodrigues.principal@gmail.com>
2020-02-10 12:02:40 -0300

    Configurando Swagger - adicionando rotas

--- server/Clients/routes.js
+++ server/Clients/routes.js
@@ -1,4 +1,5 @@
 const ClientController = require('./Controller/ClientController')
+const Joi = require('@hapi/joi')
 
 const baseRoute = '/clients'
 const routes = [
@@ -6,43 +7,98 @@ const routes = [
         method: 'GET',
         path: `${baseRoute}`,
         config: { auth: 'jwt' },
-        handler: ClientController.getClient
+        handler: ClientController.getClient,
+        config: {
+            tags: ['api'],
+            description: 'Get all Clients',
+            notes: 'Retorna todos os clientes. ATENÇÃO: Usar Paginação',
+            validate: {
+                query: Joi.object({
+                    page : Joi.number()
+                            .required()
+                            .description('the id for the Client page'),
+                })
+            }      
+        }
     },
     {
         method: 'GET',
         path: `${baseRoute}/{id}`,
         config: { auth: 'jwt' },
-        handler: ClientController.getClient
+        handler: ClientController.getClient,
+        config: {
+            tags: ['api'],
+            description: 'Get one Client',
+            notes: 'Retorna Um cliente específico',
+            validate: {
+                params: Joi.object({
+                    id : Joi.number()
+                            .required()
+                            .description('the id for the Client item'),
+                })
+            }    
+        }
     },
     {
         method: 'POST',
         path: `${baseRoute}`,
         config: { auth: 'jwt' },
-        handler: ClientController.saveClient
+        handler: ClientController.saveClient,
+        config: {
+            tags: ['api'],
+            description: 'Create new Client',
+            notes: 'Cria um novo Cliente',
+            validate: {
+                payload: Joi.object({
+                    nome: Joi.string(),
+                    email: Joi.string()
+                })
+            }    
+        }
     },
     {
         method: 'POST',
         path: `${baseRoute}/{id}/favorite`,
         config: { auth: 'jwt' },
-        handler: ClientController.addFavorite
+        handler: ClientController.addFavorite,
+        config: {
+            tags: ['api'],
+            description: 'Add Favorite To Client',
+            notes: 'Adiciona na lista de favoritos do Cliente',    
+        }
     },
     {
         method: 'PUT',
         path: `${baseRoute}/{id}`,
         config: { auth: 'jwt' },
-        handler: ClientController.updateClient
+        handler: ClientController.updateClient,
+        config: {
+            tags: ['api'],
+            description: 'Update a Client',
+            notes: 'Atualiza Cliente Existente',    
+        }
     },
     {
         method: 'DELETE',
         path: `${baseRoute}/{id}/favorite`,
         config: { auth: 'jwt' },
-        handler: ClientController.removeFavorite
+        handler: ClientController.removeFavorite,
+        config: {
+            tags: ['api'],
+            description: 'Remove Favorite To Client',
+            notes: 'Remove da lista de favoritos do Cliente',    
+        }
     },
     {
         method: 'DELETE',
         path: `${baseRoute}/{id}`,
         config: { auth: 'jwt' },
-        handler: ClientController.deleteClient
+        handler: ClientController.deleteClient,
+        config: {
+            tags: ['api'],
+            description: 'Remove Client',
+            notes: 'Remove completamente um Cliente',    
+        }
     }
 ]
 module.exports = routes
--- server/User/routes.js
+++ server/User/routes.js
@@ -1,4 +1,5 @@
 const UserController = require('./Controller/UserController')
+const Joi = require('@hapi/joi')
 
 const baseRoute = '/auth'
 const routes = [
@@ -6,7 +7,18 @@ const routes = [
         method: 'POST',
         path: `${baseRoute}/login`,
         config: { auth: false },
-        handler: UserController.findUser
+        handler: UserController.findUser,
+        config: {
+            tags: ['api'],
+            description: 'Auth',
+            notes: 'Faz o login na aplicação',
+            validate: {
+                payload: Joi.object({
+                    name: Joi.string(),
+                    password: Joi.string()
+                })
+            }    
+        }
     }
 ]
 module.exports = routes
